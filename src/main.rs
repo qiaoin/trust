8925d811641e7ed4bfbc5758bc4171acbeae1fc5
qiao <qiao.liubing@gmail.com>
2021-06-13 22:13:29 +0800

    feat: init

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+use std::io;
+use std::net::Ipv4Addr;
+
+mod tcp;
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
+
+fn main() -> io::Result<()> {
+    let mut connections: HashMap<Quad, tcp::State> = Default::default();
+
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            // not ipv4
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(iph) => {
+                let src = iph.source_addr();
+                let dst = iph.destination_addr();
+                if iph.protocol() != 0x06 {
+                    // not tcp
+                    continue;
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + iph.slice().len()..nbytes]) {
+                    Ok(tcph) => {
+                        // eth_flags + eth_proto + ip_header + tcp_header
+                        let datai = 4 + iph.slice().len() + tcph.slice().len();
+                        // HashMap
+                        // entry
+                        // or_default
+                        // =>
+                        connections.entry(Quad {
+                            src: (src, tcph.source_port()),
+                            dst: (dst, tcph.destination_port()),
+                        }).or_default().on_packet(iph, tcph, &buf[datai..nbytes]);
+                    }
+                    Err(e) => {
+                        eprintln!("ignoring wired tcp packet {:?}", e);
+                    }
+                }
+            },
+            Err(e) => {
+                eprintln!("ignoring wired packet {:?}", e);
+            }
+        }
+    }
+}
