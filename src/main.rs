47fe0e929b6f9f3a1d3cf22062158f9343973d18
qiao <qiao.liubing@gmail.com>
2021-06-14 21:37:23 +0800

    feat: part one

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,24 @@ struct Quad {
 }
 
 fn main() -> io::Result<()> {
-    let mut connections: HashMap<Quad, tcp::State> = Default::default();
+    let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
 
-    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut nic = tun_tap::Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if eth_proto != 0x0800 {
-            // not ipv4
-            continue;
-        }
 
-        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+        // if s/without_packet_info/new:
+        // let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        // let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        // if eth_proto != 0x0800 {
+        //     // not ipv4
+        //     continue;
+        // }
+        //
+        // and also include on send
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[..nbytes]) {
             Ok(iph) => {
                 let src = iph.source_addr();
                 let dst = iph.destination_addr();
@@ -33,26 +37,42 @@ fn main() -> io::Result<()> {
                     continue;
                 }
 
-                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + iph.slice().len()..nbytes]) {
+                match etherparse::TcpHeaderSlice::from_slice(&buf[iph.slice().len()..nbytes]) {
                     Ok(tcph) => {
+                        use std::collections::hash_map::Entry;
                         // eth_flags + eth_proto + ip_header + tcp_header
-                        let datai = 4 + iph.slice().len() + tcph.slice().len();
+                        let datai = iph.slice().len() + tcph.slice().len();
                         // HashMap
                         // entry
                         // or_default
                         // =>
-                        connections.entry(Quad {
+                        match connections.entry(Quad {
                             src: (src, tcph.source_port()),
                             dst: (dst, tcph.destination_port()),
-                        }).or_default().on_packet(iph, tcph, &buf[datai..nbytes]);
+                        }) {
+                            Entry::Occupied(mut c) => {
+                                c.get_mut()
+                                    .on_packet(&mut nic, iph, tcph, &buf[datai..nbytes])?;
+                            }
+                            Entry::Vacant(mut e) => {
+                                if let Some(c) = tcp::Connection::accept(
+                                    &mut nic,
+                                    iph,
+                                    tcph,
+                                    &buf[datai..nbytes],
+                                )? {
+                                    e.insert(c);
+                                }
+                            }
+                        }
                     }
                     Err(e) => {
                         eprintln!("ignoring wired tcp packet {:?}", e);
                     }
                 }
-            },
-            Err(e) => {
-                eprintln!("ignoring wired packet {:?}", e);
+            }
+            Err(_) => {
+                // eprintln!("ignoring wired packet {:?}", e);
             }
         }
     }
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,26 +1,395 @@
-pub struct State {}
+use std::io;
 
-impl Default for State {
-    fn default() -> Self {
-        State {}
-    }
+enum State {
+    // Closed,
+    // Listen,
+    SynRcvd,
+    Estab,
+    FinWait1,
+    FinWait2,
+    TimeWait,
 }
 
 impl State {
-    pub fn on_packet<'a>(
-        &mut self,
+    fn is_synchronized(&self) -> bool {
+        match *self {
+            State::SynRcvd => false,
+            State::Estab | State::FinWait1 | State::FinWait2 | State::TimeWait => true,
+        }
+    }
+}
+
+// Transmission Control Block
+pub struct Connection {
+    state: State,
+    send: SendSequenceSpace,
+    recv: RecvSequenceSpace,
+    ip: etherparse::Ipv4Header,
+    tcp: etherparse::TcpHeader,
+}
+
+/// State of Send Sequence Space (RFC 793 S3.2 Figure 4)
+/// ```
+///      1         2          3          4
+/// ----------|----------|----------|----------
+///        SND.UNA    SND.NXT    SND.UNA
+///                             +SND.WND
+///
+/// 1 - old sequence numbers which have been acknowledged
+/// 2 - sequence numbers of unacknowledged data
+/// 3 - sequence numbers allowed for new data transmission
+/// 4 - future sequence numbers which are not yet allowed
+/// ```
+struct SendSequenceSpace {
+    /// send unacknowledged
+    una: u32,
+    /// send next
+    nxt: u32,
+    /// send window
+    wnd: u16,
+    /// send urgent pointer
+    up: bool,
+    /// segment sequence number used for last window update
+    wl1: u32,
+    /// segment acknowledgment number used for last window update
+    wl2: u32,
+    /// initial send sequence number
+    iss: u32,
+}
+
+/// State of Receive Sequence Space (RFC 793 S3.2 Figure 5)
+/// ```
+///      1          2          3
+/// ----------|----------|----------
+///        RCV.NXT    RCV.NXT
+///                  +RCV.WND
+///
+/// 1 - old sequence numbers which have been acknowledged
+/// 2 - sequence numbers allowed for new reception
+/// 3 - future sequence numbers which are not yet allowed
+/// ```
+struct RecvSequenceSpace {
+    /// receive next
+    nxt: u32,
+    /// receive window
+    wnd: u16,
+    /// receive urgent pointer
+    up: bool,
+    /// initial receive sequence number
+    irs: u32,
+}
+
+// 对于不同的状态，有不同的表现，因此就没有 Default
+// impl Default for Connection {
+//     fn default() -> Self {
+//         // State::Closed
+//         Connection {
+//             state: State::Listen,
+//         }
+//     }
+// }
+
+impl Connection {
+    // 前置条件：Listen State
+    pub fn accept<'a>(
+        nic: &mut tun_tap::Iface,
         iph: etherparse::Ipv4HeaderSlice<'a>,
         tcph: etherparse::TcpHeaderSlice<'a>,
         data: &'a [u8],
-    ) {
-        // (srcip, srcport, dstip, dstport)
-        eprintln!(
-            "{}:{} -> {}:{} {}b of tcp", 
-            iph.source_addr(),
-            tcph.source_port(),
-            iph.destination_addr(),
-            tcph.destination_port(),
-            data.len(),
-        );
+    ) -> io::Result<Option<Self>> {
+        let mut buf = [0u8; 1500];
+        if !tcph.syn() {
+            // only expected SYN package
+            return Ok(None);
+        }
+
+        let iss = 0;
+        let wnd = 1024;
+        let mut c = Connection {
+            state: State::SynRcvd,
+            send: SendSequenceSpace {
+                iss,
+                una: iss,
+                nxt: iss,
+                wnd,
+
+                up: false,
+                wl1: 0,
+                wl2: 0,
+            },
+            recv: RecvSequenceSpace {
+                nxt: tcph.sequence_number() + 1,
+                wnd: tcph.window_size(),
+                irs: tcph.sequence_number(),
+
+                up: false,
+            },
+            ip: etherparse::Ipv4Header::new(
+                0,
+                64,
+                etherparse::IpTrafficClass::Tcp,
+                [
+                    iph.destination()[0],
+                    iph.destination()[1],
+                    iph.destination()[2],
+                    iph.destination()[3],
+                ],
+                [
+                    iph.source()[0],
+                    iph.source()[1],
+                    iph.source()[2],
+                    iph.source()[3],
+                ],
+            ),
+            tcp: etherparse::TcpHeader::new(tcph.destination_port(), tcph.source_port(), iss, wnd),
+        };
+
+        // need to start establishing a connection
+        c.tcp.ack = true;
+        c.tcp.syn = true;
+
+        c.write(nic, &[])?;
+        Ok(Some(c))
     }
+
+    fn write(&mut self, nic: &mut tun_tap::Iface, payload: &[u8]) -> io::Result<usize> {
+        let mut buf = [0u8; 1500];
+        self.tcp.sequence_number = self.send.nxt;
+        self.tcp.acknowledgment_number = self.recv.nxt;
+
+        let size = std::cmp::min(
+            buf.len(),
+            self.tcp.header_len() as usize + self.ip.header_len() as usize + payload.len(),
+        );
+
+        self.ip
+            .set_payload_len(size - self.ip.header_len() as usize);
+
+        // the kernel is nice and does this for us
+        self.tcp.checksum = self
+            .tcp
+            .calc_checksum_ipv4(&self.ip, payload)
+            .expect("fail to compute checksum");
+
+        // write out the headers
+        use std::io::Write;
+        let mut unwritten = &mut buf[..];
+        self.ip.write(&mut unwritten);
+        self.tcp.write(&mut unwritten);
+        let payload_bytes = unwritten.write(payload)?;
+        let unwritten = unwritten.len();
+        self.send.nxt = self.send.nxt.wrapping_add(payload_bytes as u32);
+        if self.tcp.syn {
+            self.send.nxt = self.send.nxt.wrapping_add(1);
+            self.tcp.syn = false;
+        }
+        if self.tcp.fin {
+            self.send.nxt = self.send.nxt.wrapping_add(1);
+            self.tcp.fin = false;
+        }
+        nic.send(&buf[..buf.len() - unwritten])?;
+        Ok(payload_bytes)
+    }
+
+    fn send_rst(&mut self, nic: &mut tun_tap::Iface) -> io::Result<()> {
+        self.tcp.rst = true;
+        // TODO: fix sequence number here
+        // RFC 793 Page 35
+        // If the incoming segment has an ACK field, the reset takes its
+        // sequence number from the ACK field of the segment, otherwise the
+        // reset has sequence number zero and the ACK field is set to the sum
+        // of the sequence number and segment length of the incoming segment.
+        // The connection remains in the same state.
+        //
+        // TODO: handle synchronized RST
+        // 3.  If the connection is in a synchronized state (ESTABLISHED,
+        // FIN-WAIT-1, FIN-WAIT-2, CLOSE-WAIT, CLOSING, LAST-ACK, TIME-WAIT),
+        // any unacceptable segment (out of window sequence number or
+        // unacceptible acknowledgment number) must elicit only an empty
+        // acknowledgment segment containing the current send-sequence number
+        // and an acknowledgment indicating the next sequence number expected
+        // to be received, and the connection remains in the same state.
+        self.tcp.sequence_number = 0;
+        self.tcp.acknowledgment_number = 0;
+
+        self.write(nic, &[])?;
+        Ok(())
+    }
+
+    pub fn on_packet<'a>(
+        &mut self,
+        nic: &mut tun_tap::Iface,
+        iph: etherparse::Ipv4HeaderSlice<'a>,
+        tcph: etherparse::TcpHeaderSlice<'a>,
+        data: &'a [u8],
+    ) -> io::Result<()> {
+        // first, check that sequence numbers are valid (RFC 793 S3.3)
+        //
+        // valid segment check. okey if it acks at least one byte, which means that at least
+        // one of the following is true:
+        //   - RCV.NXT =< SEG.SEQ < RCV.NXT+RCV.WND
+        //   - RCV.NXT =< SEG.SEQ+SEG.LEN-1 < RCV.NXT+RCV.WND
+        //
+        let seqn = tcph.sequence_number();
+        let mut slen = data.len() as u32;
+        if tcph.fin() || tcph.syn() {
+            slen += 1;
+        }
+        let wend = self.recv.nxt.wrapping_add(self.recv.wnd as u32);
+        if slen == 0 {
+            // zero-length segment has separate rules for acceptance
+            if self.recv.wnd == 0 {
+                if seqn != self.recv.nxt {
+                    return Ok(());
+                }
+            } else if !is_between_wrapped(self.recv.nxt.wrapping_sub(1), seqn, wend) {
+                return Ok(());
+            }
+        } else {
+            if self.recv.wnd == 0 {
+                return Ok(());
+            } else if !is_between_wrapped(self.recv.nxt.wrapping_sub(1), seqn, wend)
+                && !is_between_wrapped(
+                    self.recv.nxt.wrapping_sub(1),
+                    seqn.wrapping_add(slen - 1),
+                    wend,
+                )
+            {
+                return Ok(());
+            }
+        }
+
+        self.recv.nxt = seqn.wrapping_add(slen);
+        // TODO: make sure this get acked
+
+        // RFC 793 Page 68 SEGMENT ARRIVES
+        // TODO: if _not_ acceptable, send ACK
+        // <SEQ=SND.NXT><ACK=RCV.NXT><CTL=ACK>
+
+        if !tcph.ack() {
+            return Ok(());
+        }
+
+        let ackn = tcph.acknowledgment_number();
+        if let State::SynRcvd = self.state {
+            if is_between_wrapped(
+                self.send.una.wrapping_sub(1),
+                ackn,
+                self.send.nxt.wrapping_add(1),
+            ) {
+                // must have ACKed our SYN, since we detected at least one acked byte,
+                // and we have only sent only one byte (the SYN)
+                self.state = State::Estab;
+            } else {
+                // TODO: <SEQ=SEG.ACK><CTL=RST> Page 71
+            }
+        }
+
+        if let State::Estab | State::FinWait1 | State::FinWait2 = self.state {
+            if !is_between_wrapped(self.send.una, ackn, self.send.nxt.wrapping_add(1)) {
+                // if !self.state.is_synchronized() {
+                //     // RFC 793 S3.4 Page 35
+                //     // according to Reset Generation, we should send a RST
+                //     self.send_rst(nic)?;
+                // }
+                return Ok(());
+            }
+            self.send.una = ackn;
+            assert!(data.is_empty());
+
+            if let State::Estab = self.state {
+                // now, let's terminate the connection
+                // TODO: needs to be store in the retransmission queue!
+                self.tcp.fin = true;
+                self.write(nic, &[])?;
+                self.state = State::FinWait1;
+            }
+        }
+
+        if let State::FinWait1 = self.state {
+            if self.send.una == self.send.iss + 2 {
+                // our FIN has been ACKed
+
+                // must have ACKed our FIN, since we detected at least one acked byte,
+                // and we have only sent only one byte (the FIN)
+                self.state = State::FinWait2;
+            }
+        }
+
+        if tcph.fin() {
+            if let State::FinWait2 = self.state {
+                // we're done with the connection!
+                if !tcph.fin() && !data.is_empty() {
+                    unimplemented!();
+                }
+
+                self.write(nic, &[])?;
+                self.state = State::TimeWait;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn is_between_wrapped(start: u32, x: u32, end: u32) -> bool {
+    use std::cmp::Ordering;
+    match start.cmp(&x) {
+        Ordering::Equal => return false,
+        Ordering::Less => {
+            // we have:
+            //
+            //  0 |---------S------X---------| (wraparound)
+            //
+            // X is between S and E (S < X < E) in these cases:
+            //
+            //  0 |---------S------X---E-----| (wraparound)
+            //
+            //  0 |-----E---S------X---------| (wraparound)
+            //
+            // but *not* in these cases:
+            //
+            //  0 |---------S--E---X---------| (wraparound)
+            //
+            //  0 |---------|------X---------| (wraparound)
+            //              ^-S+E
+            //
+            //  0 |---------S------|---------| (wraparound)
+            //                 X+E-^
+            //
+            // or, in other words, iff !(S <= E <= X)
+            if start <= end && end <= x {
+                return false;
+            }
+        }
+        Ordering::Greater => {
+            // we have the opposite of above:
+            //
+            //  0 |---------X------S---------| (wraparound)
+            //
+            // X is between S and E (S < X < E) *only* in this case:
+            //
+            //  0 |---------X--E---S---------| (wraparound)
+            //
+            // but *not* in these cases:
+            //
+            //  0 |-----E---X------S---------| (wraparound)
+            //
+            //  0 |---------X------S--E------| (wraparound)
+            //
+            //  0 |---------|------S---------| (wraparound)
+            //              ^-X+E
+            //
+            //  0 |---------X------|---------| (wraparound)
+            //                 S+E-^
+            //
+            // or, in other words, iff X < E < S
+            if x < end && end < start {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    true
 }
